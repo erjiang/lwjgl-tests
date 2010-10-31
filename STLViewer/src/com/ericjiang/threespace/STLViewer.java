7b065d54c013fcce20f4d559e4eb94075217c197
erjiang <erjiang@indiana.edu>
2010-10-31 03:34:17 -0400

    STLViewer works now!

--- STLViewer/src/com/ericjiang/threespace/STLViewer.java
+++ STLViewer/src/com/ericjiang/threespace/STLViewer.java
@@ -1,4 +1,8 @@
 package com.ericjiang.threespace;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
@@ -11,6 +15,9 @@ public class STLViewer {
 	static public final int SCREEN_WIDTH = 800;
 	static public final int SCREEN_HEIGHT = 600;
 	static float MOVE_DELTA = 0;
+	static float zoomFactor = 1f;
+	
+	static Mesh mesh;
 
 	/**
 	 * @param args
@@ -18,17 +25,35 @@ public class STLViewer {
 	 * @throws LWJGLException 
 	 */
 	public static void main(String[] args) throws InterruptedException, LWJGLException {
-		// TODO Auto-generated method stub
-		Timer time = new Timer();
 		
+		if(args.length == 0) {
+			System.err.println("STLViewer displays ASCII STL files.");
+			System.err.println("Usage: STLViewer [filename.stl]");
+			System.exit(1);
+		}
+		try {
+			mesh = STLParser.parse(new FileReader(args[0]));
+		} catch (FileNotFoundException e1) {
+			System.err.println(e1.getMessage());
+			System.exit(1);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			System.exit(1);
+		} catch (STLException e1) {
+			System.err.println("Could not parse STL file.  Invalid STL file?");
+			System.err.println(e1.getMessage());
+			System.exit(1);
+		}
+		
+		Timer time = new Timer();
 		float fpstick = 1.0f / 30.0f; // gives 60fps??
 
 		try {
 			initGfx();
 		} catch (LWJGLException e) {
-			// TODO Auto-generated catch block
 			System.out.println("Could not make gfx :(");
 			e.printStackTrace();
+			System.exit(1);
 		}
 		
 		float lastTime;
@@ -58,16 +83,23 @@ public class STLViewer {
 				Timer.tick();
 				now = time.getTime();
 				timeDelta = now - lastTime;
-				MOVE_DELTA += timeDelta;
+
+                if(Keyboard.isKeyDown(Keyboard.KEY_SUBTRACT)){
+                	zoomFactor *= 1f - timeDelta;
+                } else if(Keyboard.isKeyDown(Keyboard.KEY_ADD)){
+                	zoomFactor *= 1f + timeDelta;
+                }
+                
+				MOVE_DELTA += timeDelta / 2;
 				frames++;
 				
 				render();
 				Display.update();
 				
 				// sleep until next update
-				if(fpstick - timeDelta > 0.005) {
+				/*if(fpstick - timeDelta > 0.005) {
 					Thread.sleep((long) ((fpstick - timeDelta) * 1000));
-				}
+				}*/
 				
 			}
 		}
@@ -90,6 +122,9 @@ public class STLViewer {
 		//GL11.glDepthMask(false);
 		GL11.glMatrixMode(GL11.GL_PROJECTION); // Select The Projection Matrix
 		GL11.glLoadIdentity(); // Reset The Projection Matrix
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glDepthMask(true);
 
 		GLU.gluPerspective(
 				45.0f,
@@ -109,22 +144,18 @@ public class STLViewer {
 		GL11.glLoadIdentity();
 		// move back into the screen so we can see stuff
 		
-		GL11.glTranslatef(-2.0f, 0.0f, -6.0f);
-		GL11.glRotatef(MOVE_DELTA * 360f, 0.2f, 1.0f, 0);
-		GL11.glBegin(GL11.GL_TRIANGLES);
-			GL11.glColor3f(1.0f, 0, 0);
-			GL11.glVertex3f(0.0f, 1.5f, 0.0f);
-			GL11.glColor3f(0, 1.0f, 0);
-			GL11.glVertex3f(0.0f, 0.0f, 0.0f);
-			GL11.glColor3f(0f, 0, 1.0f);
-			GL11.glVertex3f(1.0f, 0.0f, 0.0f);
-		GL11.glEnd();
-		GL11.glTranslatef(2.0f + MOVE_DELTA, 0.0f, 0.0f);
-		GL11.glBegin(GL11.GL_QUADS);
-			GL11.glVertex3f(0.0f, 1.5f, 0.0f);
-			GL11.glVertex3f(0.0f, 0.0f, 0.0f);
-			GL11.glVertex3f(1.0f, 0.0f, 0.0f);
-			GL11.glVertex3f(1.0f, 1.2f, 0.0f);
-		GL11.glEnd();
+		GL11.glTranslatef(0f, 0.0f, -6.0f);
+		GL11.glScalef(zoomFactor, zoomFactor, zoomFactor);
+		GL11.glRotatef(MOVE_DELTA * 360f, 0.5f, 1.0f, 0f);
+		for(Triangle tri : mesh) {
+			GL11.glBegin(GL11.GL_TRIANGLES);
+				GL11.glColor3f(1.0f, 0, 0);
+				GL11.glVertex3f(tri.vertices[0].x, tri.vertices[0].y, tri.vertices[0].z);
+				GL11.glColor3f(0, 1.0f, 0);
+				GL11.glVertex3f(tri.vertices[1].x, tri.vertices[1].y, tri.vertices[1].z);
+				GL11.glColor3f(0f, 0, 1.0f);
+				GL11.glVertex3f(tri.vertices[2].x, tri.vertices[2].y, tri.vertices[2].z);
+			GL11.glEnd();
+		}
 	}
 }
